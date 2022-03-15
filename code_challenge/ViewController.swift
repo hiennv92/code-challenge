cdb2d65a24d6d561c18ea99a866f564de8719b80
Hien Nguyen <nguyenviethien.1992@gmail.com>
2022-03-16 05:20:22 +0700

    Initial Commit

---
+++ code_challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  code_challenge
+//
+//  Created by Hien Nguyen on 16/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
